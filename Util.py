15e10ebb342509f5ea19a77c725d63e736541e56
rmslick <rmslick@buffalo.edu>
2023-10-11 17:51:32 -0400

    adding utility functions for testing and callbacks for training

--- Util.py
+++ Util.py
@@ -4,6 +4,86 @@ import cv2
 import numpy as np
 from scipy.spatial.transform import Rotation
 from .RGBCNN import *
+from tensorflow.keras.preprocessing import image
+
+def get_ground_truth_pose_for_image(image_filename, json_path):
+    """
+    Retrieve the ground truth 6DoF pose for a given image from the LineMOD dataset.
+
+    Parameters:
+    - image_filename: The filename of the image, e.g., '000123.jpg'.
+    - json_path: Path to the scene_gt.json file for the corresponding scene.
+
+    Returns:
+    - pose: A dictionary containing the ground truth pose (rotation and translation).
+    """
+
+    # Extract image ID from the filename
+    image_id = int(image_filename.split('.')[0])
+
+    # Load the JSON data
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+
+    # Extract pose for the given image ID
+    pose_data = data[str(image_id)][0]  # Assuming one primary object per image in LineMOD
+    rotation_matrix = pose_data['cam_R_m2c']
+    translation_vector = pose_data['cam_t_m2c']
+
+    pose = {
+        'rotation': rotation_matrix,
+        'translation': translation_vector
+    }
+
+    return pose
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+def dict_to_7d_format(pose_dict):
+    """
+    Convert a dictionary with 'rotation' and 'translation' keys to a 7D array format:
+    [tx, ty, tz, qx, qy, qz, qw].
+    """
+    R = np.array(pose_dict['rotation']).reshape(3, 3)  # Convert rotation list to a 3x3 matrix.
+
+    # Convert the rotation matrix to a quaternion.
+    rotation = Rotation.from_matrix(R)
+    quaternion = rotation.as_quat()
+
+    # Combine translation and quaternion into a single array.
+    pose_7d = np.concatenate([pose_dict['translation'], quaternion])
+
+    return pose_7d
+import numpy as np
+
+def pose_error(gt_pose, pred_pose):
+    """
+    Calculate and print the translation and rotation error between a ground truth pose and a predicted pose.
+
+    Parameters:
+    - gt_pose: Ground truth pose as a 7D array [tx, ty, tz, qx, qy, qz, qw].
+    - pred_pose: Predicted pose as a 7D array [tx, ty, tz, qx, qy, qz, qw].
+    """
+
+    # Translation error
+    trans_error = np.linalg.norm(gt_pose[:3] - pred_pose[:3])
+
+    # Rotation error
+    q_gt = gt_pose[3:]
+    q_pred = pred_pose[3:]
+    dot_product = np.dot(q_gt, q_pred)
+
+    # Clip to ensure dot_product is within the valid range for arccos
+    dot_product = np.clip(dot_product, -1.0, 1.0)
+
+    rotation_error = 2 * np.arccos(np.abs(dot_product))
+
+    #print(f"Translation Error: {trans_error:.4f} units")
+    #print(f"Rotation Error: {rotation_error:.4f} radians")
+
+    return trans_error, rotation_error
+
+
 def poses_to_quaternion_format(poses):
     """
     Convert a list of poses in the format:
